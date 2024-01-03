2a171c39dbb90d27d92fe65331c0955811292ae1
skdud907 <skdud907@naver.com>
2024-01-03 16:52:26 +0900

    [ADD] 파일 분리

--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:splash_screen/screen/home_screen.dart';
 
 void main() {
   runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
     home: HomeScreen(),
   ));
-}
-
-class HomeScreen extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Image.asset(
-            'asset/img/logo.png',
-          ),
-          CircularProgressIndicator(
-            color: Colors.amberAccent,
-          ),
-        ],
-      ),
-    );
-  }
 }
---
+++ lib/screen/home_screen.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Image.asset(
+            'asset/img/logo.png',
+          ),
+          CircularProgressIndicator(
+            color: Colors.amberAccent,
+          ),
+        ],
+      ),
+    );
+  }
+}
